9e3a3c2b0d216908dea0a50a5d7788bacd273d8a
Łukasz Zalewski <zalewszczak@gmail.com>
2013-04-22 05:10:36 +0200

    fixed font, added conditions

--- classic/src/classic.c
+++ classic/src/classic.c
@@ -6,9 +6,13 @@
 #define MY_UUID { 0xAD, 0x80, 0x11, 0xA9, 0x02, 0xB5, 0x4C, 0x88, 0x99, 0xF8, 0x88, 0xF4, 0x38, 0xAE, 0x8B, 0xC2 }
 PBL_APP_INFO(MY_UUID,
              "Classic", "Zalew",
-             2, 0, /* App version */
+             2, 1, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
+
+#define DISPLAY_SECONDS true
+#define DISPLAY_TIME true
+#define HOUR_VIBRATION true
 
 Window window;
 
@@ -17,10 +21,18 @@ BmpContainer background_image_container;
 Layer minute_display_layer;
 Layer hour_display_layer;
 Layer center_display_layer;
-Layer second_display_layer;
+GFont roboto;
 TextLayer date_layer;
+static char date_text[] = "12";
 
-static char date_text[] = "12-04-13";
+#if DISPLAY_SECONDS
+Layer second_display_layer;
+#endif
+
+#if DISPLAY_TIME
+TextLayer time_layer;
+static char time_text[] = "12:34";
+#endif
 
 const GPathInfo MINUTE_HAND_PATH_POINTS = {
   4,
@@ -89,6 +101,7 @@ GPath minute_hand_path;
 GPath minute_hand_outline_path;
 GPath minute_hand_line_path;
 
+#if DISPLAY_SECONDS
 void second_display_layer_update_callback(Layer *me, GContext* ctx) {
   (void)me;
 
@@ -106,6 +119,7 @@ void second_display_layer_update_callback(Layer *me, GContext* ctx) {
 
   graphics_draw_line(ctx, center, second);
 }
+#endif
 
 void center_display_layer_update_callback(Layer *me, GContext* ctx) {
   (void)me;
@@ -165,6 +179,25 @@ void draw_date(){
   text_layer_set_text(&date_layer, date_text);
 }
 
+#if DISPLAY_TIME
+void draw_time(){
+  PblTm t;
+  get_time(&t);
+
+  // section based on Simplicity by Pebble Team begins
+  char *time_format;
+  if (clock_is_24h_style()) {
+     time_format = "%R";
+  } else {
+     time_format = "%I:%M";
+  }
+  string_format_time(time_text, sizeof(time_text), time_format, &t);
+  // section ends
+
+  text_layer_set_text(&time_layer, time_text);
+}
+#endif
+
 void handle_init(AppContextRef ctx) {
   (void)ctx;
 
@@ -172,10 +205,26 @@ void handle_init(AppContextRef ctx) {
   window_stack_push(&window, true /* Animated */);
 
   resource_init_current_app(&APP_RESOURCES);
+#if DISPLAY_TIME
   bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND, &background_image_container);
+#else
+  bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND_LOGO, &background_image_container);
+#endif
   layer_add_child(&window.layer, &background_image_container.layer.layer);
 
-  GFont roboto = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_THIN_11));
+  roboto = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_THIN_11));
+
+#if DISPLAY_TIME
+  text_layer_init(&time_layer, GRect(58, 42, 29, 14));
+  text_layer_set_text_alignment(&time_layer, GTextAlignmentCenter);
+  text_layer_set_text_color(&time_layer, GColorWhite);
+  text_layer_set_background_color(&time_layer, GColorClear);
+  text_layer_set_font(&time_layer, roboto);
+  layer_add_child(&window.layer, &time_layer.layer);
+
+  draw_time();
+#endif
+
   text_layer_init(&date_layer, GRect(64, 109, 16, 14));
   text_layer_set_text_alignment(&date_layer, GTextAlignmentCenter);
   text_layer_set_text_color(&date_layer, GColorWhite);
@@ -211,45 +260,53 @@ void handle_init(AppContextRef ctx) {
   center_display_layer.update_proc = &center_display_layer_update_callback;
   layer_add_child(&window.layer, &center_display_layer);
 
+#if DISPLAY_SECONDS
   layer_init(&second_display_layer, window.layer.frame);
   second_display_layer.update_proc = &second_display_layer_update_callback;
   layer_add_child(&window.layer, &second_display_layer);
+#endif
 }
 
 void handle_deinit(AppContextRef ctx) {
   (void)ctx;
 
   bmp_deinit_container(&background_image_container);
+  fonts_unload_custom_font(roboto);
 }
 
-void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t){
+void handle_tick(AppContextRef ctx, PebbleTickEvent *t){
   (void)t;
   (void)ctx;
 
-  if(t->tick_time->tm_sec!=0)
+  if(t->tick_time->tm_sec%10==0)
   {
-     if(t->tick_time->tm_sec%10==0)
-     {
-        layer_mark_dirty(&minute_display_layer);
-     }
-     layer_mark_dirty(&second_display_layer);
-  }
-  else if(t->tick_time->tm_min!=0&&t->tick_time->tm_hour!=0)
-  {
-     if(t->tick_time->tm_min%2==0)
-     {
-        layer_mark_dirty(&hour_display_layer);
-     }
-     layer_mark_dirty(&second_display_layer);
      layer_mark_dirty(&minute_display_layer);
+     
+     if(t->tick_time->tm_sec==0)
+     {
+        if(t->tick_time->tm_min%2==0)
+        {
+           layer_mark_dirty(&hour_display_layer);
+           if(t->tick_time->tm_min==0&&t->tick_time->tm_hour==0)
+           {
+              draw_date();
+           }
+        }
+#if DISPLAY_TIME
+        draw_time();
+#endif
+#if HOUR_VIBRATION
+        if(t->tick_time->tm_min==0)
+        {
+           vibes_short_pulse();
+        }
+#endif
+    }
   }
-  else
-  {
-     layer_mark_dirty(&hour_display_layer);
-     layer_mark_dirty(&second_display_layer);
-     layer_mark_dirty(&minute_display_layer);
-     draw_date();
-  }
+
+#if DISPLAY_SECONDS
+  layer_mark_dirty(&second_display_layer);
+#endif
 }
 
 void pbl_main(void *params) {
@@ -257,8 +314,12 @@ void pbl_main(void *params) {
     .init_handler = &handle_init,
     .deinit_handler = &handle_deinit,
     .tick_info = {
-			.tick_handler = &handle_second_tick,
+			.tick_handler = &handle_tick,
+#if DISPLAY_SECONDS
 			.tick_units = SECOND_UNIT
+#else
+			.tick_units = MINUTE_UNIT
+#endif
 		}
   };
   app_event_loop(params, &handlers);
