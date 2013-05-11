a56aeb296b920d2eff585055e203279d31700d38
Łukasz Zalewski <zalewszczak@gmail.com>
2013-05-11 22:01:27 +0200

    sum files

---
+++ maurice/src/lang.h
@@ -0,0 +1,421 @@
+/*
+ * 1 Albanian
+ * 25 Catalan
+ * 32 Croatian
+ * 34 Czech
+ * 36 Danish
+ * 38 Dutch
+ * 41 English
+ * 53 Estonian
+ * 55 Finnish
+ * 57 French
+ * 63 German
+ * 74 Hungarian
+ * 76 Icelandic
+ * 78 Indonesian
+ * 80 Irish
+ * 82 Italian
+ * 90 Latvian
+ * 92 Lithuanian
+ * 96 Malay
+ * 98 Maltese
+ * 100 Norwegian
+ * 103 Polish
+ * 105 Portuguese
+ * 108 Romanian
+ * 121 Slovak
+ * 123 Slovenian
+ * 125 Spanish
+ * 146 Swedish
+ * 151 Turkish
+ * 155 Vietnamese
+ */
+#if LANG == 1
+// Albanian
+static const char* const DAYS[] = {
+"DIE", /* Die */
+"HEN", /* Hën */
+"MAR", /* Mar */
+"MER", /* Mër */
+"ENJ", /* Enj */
+"PRE", /* Pre */
+"SHT", /* Sht */
+};
+#endif 
+
+#if LANG == 25
+// Catalan
+static const char* const DAYS[] = {
+"DG.", /* dg. */
+"DL.", /* dl. */
+"DT.", /* dt. */
+"DC.", /* dc. */
+"DJ.", /* dj. */
+"DV.", /* dv. */
+"DS.", /* ds. */
+};
+#endif 
+
+#if LANG == 32
+// Croatian
+static const char* const DAYS[] = {
+"NED", /* ned */
+"PON", /* pon */
+"UTO", /* uto */
+"SRI", /* sri */
+"CET", /* čet */
+"PET", /* pet */
+"SUB", /* sub */
+};
+#endif 
+
+#if LANG == 34
+// Czech
+static const char* const DAYS[] = {
+"NE", /* Ne */
+"PO", /* Po */
+"UT", /* Út */
+"ST", /* St */
+"CT", /* Čt */
+"PA", /* Pá */
+"SO", /* So */
+};
+#endif 
+
+#if LANG == 36
+// Danish
+static const char* const DAYS[] = {
+"S", /* sø */
+"MA", /* ma */
+"TI", /* ti */
+"ON", /* on */
+"TO", /* to */
+"FR", /* fr */
+"L", /* lø */
+};
+#endif 
+
+#if LANG == 38
+// Dutch
+static const char* const DAYS[] = {
+"ZO", /* zo */
+"MA", /* ma */
+"DI", /* di */
+"WO", /* wo */
+"DO", /* do */
+"VR", /* vr */
+"ZA", /* za */
+};
+#endif 
+
+#if LANG == 41
+// English
+static const char* const DAYS[] = {
+"SUN", /* Sun */
+"MON", /* Mon */
+"TUE", /* Tue */
+"WED", /* Wed */
+"THU", /* Thu */
+"FRI", /* Fri */
+"SAT", /* Sat */
+};
+#endif 
+
+#if LANG == 53
+// Estonian
+static const char* const DAYS[] = {
+"P", /* P */
+"E", /* E */
+"T", /* T */
+"K", /* K */
+"N", /* N */
+"R", /* R */
+"L", /* L */
+};
+#endif 
+
+#if LANG == 55
+// Finnish
+static const char* const DAYS[] = {
+"SU", /* su */
+"MA", /* ma */
+"TI", /* ti */
+"KE", /* ke */
+"TO", /* to */
+"PE", /* pe */
+"LA", /* la */
+};
+#endif 
+
+#if LANG == 57
+// French
+static const char* const DAYS[] = {
+"DIM.", /* dim. */
+"LUN.", /* lun. */
+"MAR.", /* mar. */
+"MER.", /* mer. */
+"JEU.", /* jeu. */
+"VEN.", /* ven. */
+"SAM.", /* sam. */
+};
+#endif 
+
+#if LANG == 63
+// German
+static const char* const DAYS[] = {
+"SO", /* So */
+"MO", /* Mo */
+"DI", /* Di */
+"MI", /* Mi */
+"DO", /* Do */
+"FR", /* Fr */
+"SA", /* Sa */
+};
+#endif 
+
+#if LANG == 74
+// Hungarian
+static const char* const DAYS[] = {
+"V", /* V */
+"H", /* H */
+"K", /* K */
+"SZE", /* Sze */
+"CS", /* Cs */
+"P", /* P */
+"SZO", /* Szo */
+};
+#endif 
+
+#if LANG == 76
+// Icelandic
+static const char* const DAYS[] = {
+"SUN.", /* sun. */
+"MAN.", /* mán. */
+"RI.", /* þri. */
+"MI.", /* mið. */
+"FIM.", /* fim. */
+"FOS.", /* fös. */
+"LAU.", /* lau. */
+};
+#endif 
+
+#if LANG == 78
+// Indonesian
+static const char* const DAYS[] = {
+"MIN", /* Min */
+"SEN", /* Sen */
+"SEL", /* Sel */
+"RAB", /* Rab */
+"KAM", /* Kam */
+"JUM", /* Jum */
+"SAB", /* Sab */
+};
+#endif 
+
+#if LANG == 80
+// Irish
+static const char* const DAYS[] = {
+"DOMH", /* Domh */
+"LUAN", /* Luan */
+"MAIRT", /* Máirt */
+"CEAD", /* Céad */
+"DEAR", /* Déar */
+"AOINE", /* Aoine */
+"SATH", /* Sath */
+};
+#endif 
+
+#if LANG == 82
+// Italian
+static const char* const DAYS[] = {
+"DOM", /* dom */
+"LUN", /* lun */
+"MAR", /* mar */
+"MER", /* mer */
+"GIO", /* gio */
+"VEN", /* ven */
+"SAB", /* sab */
+};
+#endif 
+
+#if LANG == 90
+// Latvian
+static const char* const DAYS[] = {
+"SV", /* Sv */
+"P", /* P */
+"O", /* O */
+"T", /* T */
+"C", /* C */
+"PK", /* Pk */
+"S", /* S */
+};
+#endif 
+
+#if LANG == 92
+// Lithuanian
+static const char* const DAYS[] = {
+"SK", /* Sk */
+"PR", /* Pr */
+"AN", /* An */
+"TR", /* Tr */
+"KT", /* Kt */
+"PN", /* Pn */
+"ST", /* Št */
+};
+#endif 
+
+#if LANG == 96
+// Malay
+static const char* const DAYS[] = {
+"AHD", /* Ahd */
+"ISN", /* Isn */
+"SEL", /* Sel */
+"RAB", /* Rab */
+"KHA", /* Kha */
+"JUM", /* Jum */
+"SAB", /* Sab */
+};
+#endif 
+
+#if LANG == 98
+// Maltese
+static const char* const DAYS[] = {
+"AD", /* Ħad */
+"TNE", /* Tne */
+"TLI", /* Tli */
+"ERB", /* Erb */
+"AM", /* Ħam */
+"GIM", /* Ġim */
+"SIB", /* Sib */
+};
+#endif 
+
+#if LANG == 100
+// Norwegian
+static const char* const DAYS[] = {
+"S", /* sø */
+"MA", /* ma */
+"TI", /* ti */
+"ON", /* on */
+"TO", /* to */
+"FR", /* fr */
+"L", /* lø */
+};
+#endif 
+
+#if LANG == 103
+// Polish
+static const char* const DAYS[] = {
+"N", /* N */
+"PN", /* Pn */
+"WT", /* Wt */
+"SR", /* Śr */
+"CZ", /* Cz */
+"PT", /* Pt */
+"SO", /* So */
+};
+#endif 
+
+#if LANG == 105
+// Portuguese
+static const char* const DAYS[] = {
+"DOM", /* Dom */
+"SEG", /* Seg */
+"TER", /* Ter */
+"QUA", /* Qua */
+"QUI", /* Qui */
+"SEX", /* Sex */
+"SAB", /* Sáb */
+};
+#endif 
+
+#if LANG == 108
+// Romanian
+static const char* const DAYS[] = {
+"D", /* D */
+"L", /* L */
+"MA", /* Ma */
+"MI", /* Mi */
+"J", /* J */
+"V", /* V */
+"S", /* S */
+};
+#endif 
+
+#if LANG == 121
+// Slovak
+static const char* const DAYS[] = {
+"NE", /* Ne */
+"PO", /* Po */
+"UT", /* Ut */
+"ST", /* St */
+"ST", /* Št */
+"PI", /* Pi */
+"SO", /* So */
+};
+#endif 
+
+#if LANG == 123
+// Slovenian
+static const char* const DAYS[] = {
+"NED", /* Ned */
+"PON", /* Pon */
+"TOR", /* Tor */
+"SRE", /* Sre */
+"CET", /* Čet */
+"PET", /* Pet */
+"SOB", /* Sob */
+};
+#endif 
+
+#if LANG == 125
+// Spanish
+static const char* const DAYS[] = {
+"DOM", /* dom */
+"LUN", /* lun */
+"MAR", /* mar */
+"MIE", /* mié */
+"JUE", /* jue */
+"VIE", /* vie */
+"SAB", /* sáb */
+};
+#endif 
+
+#if LANG == 146
+// Swedish
+static const char* const DAYS[] = {
+"SO", /* sö */
+"MA", /* må */
+"TI", /* ti */
+"ON", /* on */
+"TO", /* to */
+"FR", /* fr */
+"LO", /* lö */
+};
+#endif 
+
+#if LANG == 151
+// Turkish
+static const char* const DAYS[] = {
+"PAZ", /* Paz */
+"PZT", /* Pzt */
+"SAL", /* Sal */
+"CAR", /* Çar */
+"PER", /* Per */
+"CUM", /* Cum */
+"CMT", /* Cmt */
+};
+#endif 
+
+#if LANG == 155
+// Vietnamese
+static const char* const DAYS[] = {
+"CN", /* CN */
+"TH 2", /* Th 2 */
+"TH 3", /* Th 3 */
+"TH 4", /* Th 4 */
+"TH 5", /* Th 5 */
+"TH 6", /* Th 6 */
+"TH 7", /* Th 7 */
+};
+#endif 
